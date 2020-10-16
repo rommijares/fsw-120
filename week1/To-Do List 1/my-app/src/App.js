d1392ca371f43f72e0c63e06e4579d85f75fd672
rommijares <romuel.mijares@bryanuniversity.edu>
2020-10-16 16:20:40 -0700

    initial

---
+++ week1/To-Do List 1/my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <input type='checkbox'/>
+      <p>Mow the lawn</p>
+
+      <input type='checkbox'/>
+      <p>Go grocery shopping</p>
+
+      <input type='checkbox'/>
+      <p>Walk the dog</p>
+
+      <input type='checkbox'/>
+      <p>Pick up dry cleaners</p>
+  </div>
+  );
+}
+
+export default App;
